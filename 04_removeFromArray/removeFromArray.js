d59603d19e82ed5547fc814c4ff2af7a13b804b8
sovationboss <bartholomaeus.philipp@gmail.com>
2023-09-15 10:33:49 +0200

    Did finish the first five exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for(const arg of args) {
+        if(arr.includes(arg)){  //same as: if(arr.indexOf(arg) >= 0)
+            let indexToRemove = arr.indexOf(arg);
+            arr.splice(indexToRemove, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+    let sum = 0;
 
+    //returns "ERROR" when the either parameter is not a number
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+
+    //"Orders" the two parameters from mix to max
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+
+    //returns "ERROR" when the either parameter is negative
+    if(min < 0 || max < 0) return "ERROR";
+
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
